c712f8f422ec1c179641e5c124821e1325cdb97a
司徒仲豪 <50611800+danielchan-25@users.noreply.github.com>
2023-01-30 17:46:55 +0800

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -23,26 +23,14 @@ yum install -y curl
 curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun
 mkdir -p ${docker_file}
 
-tee ${docker_file}/daemon.json <<-'EOF'
-{
-  "registry-mirrors": [
-    "https://0b27f0a81a00f3560fbdc00ddd2f99e0.mirror.swr.myhuaweicloud.com",
-    "https://ypzju6vq.mirror.aliyuncs.com",
-    "https://registry.docker-cn.com",
-    "http://hub-mirror.c.163.com",
-    "https://docker.mirrors.ustc.edu.cn"
-  ]
-}
-EOF
-
-systemctl enable docker && systemctl restart docker
-docker pull whyour/qinglong:latest
+systemctl enable docker && systemctl start docker
+docker pull whyour/qinglong
 
 docker run -dit \
 -v ${docker_file}/config:/ql/config \
 -v ${docker_file}/log:/ql/log \
 -v ${docker_file}/db:/ql/db -p 5700:5700 \
---name ql --restart always whyour/qinglong:latest
+--name ql --restart always whyour/qinglong
 
 sleep 10
 
